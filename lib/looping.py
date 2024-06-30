ff37508c61dbaa90dda266cf5ad82abcee365ceb
Chandler Abril <abrilchandler1@gmail.com>
2024-06-30 14:23:22 -0700

    Done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    num = 10
+    while num > 0:
+        print(num)
+        num -= 1
+    print("Happy New Year!")
+    return 
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = [(int) ** 2 for int in int_list]
+    print(squared_list)
+    return squared_list
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    num = 0
+    while num < 100:
+        num += 1 
+        if num % 15 == 0:
+            print("FizzBuzz")
+        elif num % 5 == 0:
+            print("Buzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        else: 
+            print(num)
+    
+
